223a7f4badfb6273539e493af35846de6ea9b371
Elard Huaraca <56555556+jajanotecreo@users.noreply.github.com>
2022-03-02 02:55:59 -0500

    Implement CRUD in Api Product

--- APIPrueba1DD/Controllers/api/ProductController.cs
+++ APIPrueba1DD/Controllers/api/ProductController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace APIPrueba1DD.Controllers.api
 {
@@ -11,7 +12,8 @@ namespace APIPrueba1DD.Controllers.api
     {
         private BProduct bProduct = new BProduct();
 
-        public IEnumerable<Product> Get()
+        [ResponseType(typeof(List<Product>))]
+        public IHttpActionResult GetProductActivate()
         {
             var response = new List<Product>();
             var products = bProduct.GetProductsActivate();
@@ -23,10 +25,11 @@ namespace APIPrueba1DD.Controllers.api
                 ProductExpiration = Product.reformatDate(product.ProductExpiration),
                 ProductRegistered = Product.reformatDate(product.ProductRegistered)
             }).ToList();
-            return response;
+            return Ok(response);
         }
 
-        public IEnumerable<Product> GetDesactive()
+        [ResponseType(typeof(List<Product>))]
+        public IHttpActionResult GetProductDesactivate()
         {
             var response = new List<Product>();
             var products = bProduct.GetProductsDesactive();
@@ -38,18 +41,90 @@ namespace APIPrueba1DD.Controllers.api
                 ProductExpiration = Product.reformatDate(product.ProductExpiration),
                 ProductRegistered = Product.reformatDate(product.ProductRegistered)
             }).ToList();
-            return response;
+            return Ok(response);
         }
 
-        public void Post([FromBody] Product request)
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult PostProduct(Product product)
         {
-            bProduct.InsertProduct(new Entity.Product
+            if (!ModelState.IsValid)
             {
-                ProductName = request.ProductName,
-                ProductInventory = request.ProductInventory,
-                ProductExpiration = DateTime.ParseExact(request.ProductExpiration, "MM/dd/yyyy", null),
-                ProductRegistered = DateTime.ParseExact(request.ProductRegistered,"MM/dd/yyyy HH:mm:ss", null)
+                return BadRequest(ModelState);
+            }
+
+            bool result = bProduct.InsertProduct(new Entity.Product
+            {
+                ProductName = product.ProductName,
+                ProductInventory = product.ProductInventory,
+                ProductExpiration = DateTime.ParseExact(product.ProductExpiration, "MM/dd/yyyy", null),
+                ProductRegistered = DateTime.ParseExact(product.ProductRegistered, "MM/dd/yyyy HH:mm:ss", null)
             });
+
+            if (!result)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProduct(int id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != product.ProductID)
+            {
+                return BadRequest();
+            }
+            bool result = bProduct.UpdateProduct(new Entity.Product
+            {
+                ProductID = product.ProductID,
+                ProductName = product.ProductName,
+                ProductInventory = product.ProductInventory,
+                ProductExpiration = DateTime.ParseExact(product.ProductExpiration, "MM/dd/yyyy", null),
+                ProductRegistered = DateTime.ParseExact(product.ProductRegistered, "MM/dd/yyyy HH:mm:ss", null)
+            });
+            if (!result)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProductDesactivate(int id)
+        {
+            bool result = bProduct.UpdateProductDesactivate(id);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProductActivate(int id)
+        {
+            bool result = bProduct.UpdateProductActivate(id);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [ResponseType(typeof(void))]
+        public IHttpActionResult DeleteProduct(int id)
+        {
+            bool result = bProduct.DeleteProduc(id);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return Ok();
         }
 
     }
